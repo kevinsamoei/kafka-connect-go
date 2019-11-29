e4a905a70ba264263d2795e233cdafeb72c999ff
Kevin Samoei <samoeikev@gmail.com>
2019-11-29 18:38:25 +0300

    Integration tests

--- connect_e2e_test.go
+++ connect_e2e_test.go
@@ -1,75 +1,346 @@
 package connect
 
-// FIXME: Tests are yet to be implemented
-
 import (
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
-type ConnectTestSuite struct {
-	suite.Suite
+var (
+	connectURL = "connect:8083"
+	testConnectorConfig = map[string]interface{}{
+		"connector.class": "io.confluent.connect.replicator.ReplicatorSourceConnector",
+		"topic.whitelist": "users",
+		"key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+		"value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+		"src.kafka.bootstrap.servers": "source-kafka:9091",
+		"src.consumer.group.id": "replicator-dc1-to-dc2-topic1",
+		"src.consumer.interceptor.classes": "io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor",
+		"src.consumer.confluent.monitoring.interceptor.bootstrap.servers": "destination-kafka:9092",
+		"src.kafka.timestamps.producer.interceptor.classes": "io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor",
+		"src.kafka.timestamps.producer.confluent.monitoring.interceptor.bootstrap.servers": "destination-kafka:9092",
+		"dest.kafka.bootstrap.servers": "destination-kafka:9092",
+		"confluent.topic.replication.factor": 1,
+		"provenance.header.enable": "true",
+		"header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+		"tasks.max": "1",
+	}
+)
+
+func sleep()  {
+	time.Sleep(20 * time.Second)
 }
 
-func TestConnectTestSuite(t *testing.T)  {
-	suite.Run(t, new(ConnectTestSuite))
+func TestCreateConnectorRequest(t *testing.T) {
+	// create connect
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-create-connector-request",
+		Config: testConnectorConfig,
+	})
+
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Name, req.Name)
+	assert.Equal(t, response.Config, req.Config)
+	assert.Equal(t, response.Code, 201)
 }
 
-func (suite *ConnectTestSuite) SetupTest()  {}
+func TestGetConnectors(t *testing.T) {
+	// create a connector
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-get-connectors",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
 
-func (suite *ConnectTestSuite) TearDownTest()  {}
+	// sleep to allow for rebalance
+	sleep()
 
-func (suite *ConnectTestSuite) TestCreateConnectorRequest() {
-	suite.Require().Equal(nil, nil)
+	// get connector
+	getConnectorResp, err := connect.GetConnectors()
+	assert.NoError(t, err)
+	assert.Equal(t, getConnectorResp.Code, 200)
+	assert.Contains(t, getConnectorResp.Connectors, req.Name)
+
 }
-func(suite *ConnectTestSuite) TestGetConnectors() {
-	suite.T().Log("Testing get all connectors")
-	suite.Require().Equal(nil, nil)
+
+func TestCreateConnector(t *testing.T) {
+	connect := NewConnect(connectURL)
+
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-create-connector",
+		Config: testConnectorConfig,
+	})
+
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
 }
-func (suite *ConnectTestSuite) CreateConnector() {
-	suite.T().Log("Testing Create connectors")
-	suite.Require().Equal(nil, nil)
+
+func TestGetConnector(t *testing.T) {
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-get-connector",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// get the created connector
+	getConnectorResponse, err := connect.GetConnector(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, getConnectorResponse.Code, 200)
+	assert.Equal(t, getConnectorResponse.Name, response.Name)
 }
-func (suite *ConnectTestSuite) GetConnector() {
-	suite.T().Log("Testing get single connectors")
-	suite.Require().Equal(nil, nil)
+
+func TestGetConnectorConfig(t *testing.T) {
+
+	// create a connector
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-get-connector-config",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// get it's config
+	getConfigResp, err := connect.GetConnectorConfig(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, getConfigResp.Code, 200)
+	assert.Equal(t, getConfigResp.Config, req.Config)
 }
-func (suite *ConnectTestSuite) GetConnectorConfig() {
-	suite.T().Log("Testing get connector config")
-	suite.Require().Equal(nil, nil)
+
+func TestConnect_UpdateConnectorConfig(t *testing.T) {
+	// create connector
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-update-connector-config",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// create update config request
+	updateConfig := map[string]interface{}{
+		"connector.class": "io.confluent.connect.replicator.ReplicatorSourceConnector",
+		"topic.whitelist": "users",
+		"key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+		"value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+		"src.kafka.bootstrap.servers": "source-kafka:9091",
+		"src.consumer.group.id": "replicator-dc1-to-dc2-topic1",
+		"src.consumer.interceptor.classes": "io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor",
+		"src.consumer.confluent.monitoring.interceptor.bootstrap.servers": "destination-kafka:9092",
+		"src.kafka.timestamps.producer.interceptor.classes": "io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor",
+		"src.kafka.timestamps.producer.confluent.monitoring.interceptor.bootstrap.servers": "destination-kafka:9092",
+		"dest.kafka.bootstrap.servers": "destination-kafka:9092",
+		"confluent.topic.replication.factor": 1,
+		"provenance.header.enable": "true",
+		"header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+		"tasks.max": "10",
+	}
+
+	updateReq := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   req.Name,
+		Config: updateConfig,
+	})
+
+	// update the connector config
+	updateConnectorResp, err := connect.UpdateConnectorConfig(updateReq)
+	assert.NoError(t, err)
+	assert.Equal(t, updateConnectorResp.Code, 200)
+	assert.Equal(t, updateConnectorResp.Config, updateConfig)
+	assert.Equal(t, updateConnectorResp.Config["tasks.max"], "10")
 }
-func (suite *ConnectTestSuite) UpdateConnectorConfig(){
-	suite.T().Log("Testing update connector config")
-	suite.Require().Equal(nil, nil)
+
+func TestConnect_GetConnectorStatus(t *testing.T) {
+	// create a connector
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-get-connector-status",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// get it's status
+	statusResp, err := connect.GetConnectorStatus(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, statusResp.Code, 200)
+	assert.Equal(t, statusResp.ConnectorStatus["state"], "RUNNING")
 }
-func (suite *ConnectTestSuite) GetConnectorStatus(){
-	suite.T().Log("Testing get connector status")
-	suite.Require().Equal(nil, nil)
+
+func TestConnect_RestartConnector(t *testing.T) {
+	// create connector
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-restart-connector",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// restart it
+	restartResp, err := connect.RestartConnector(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, restartResp.Code, 200)
 }
-func (suite *ConnectTestSuite) RestartConnector(){
-	suite.Require().Equal(nil, nil)
+
+func TestConnect_PauseConnector(t *testing.T) {
+	// create connector
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-pause-connector",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// pause the connector
+	pauseRes, err := connect.PauseConnector(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, pauseRes.Code, 202)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// get its status. Should be PAUSED
+	getStatusRes, err := connect.GetConnectorStatus(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, getStatusRes.Code, 200)
+	assert.Equal(t, getStatusRes.ConnectorStatus["state"], "PAUSED")
 }
-func (suite *ConnectTestSuite)PauseConnector(){
-	suite.Require().Equal(nil, nil)
+
+func TestConnect_ResumeConnector(t *testing.T) {
+	// create connector
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-restart-connector",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// pause the connector
+	pauseRes, err := connect.PauseConnector(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, pauseRes.Code, 202)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// get its status. Should be PAUSED
+	getStatusRes, err := connect.GetConnectorStatus(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, getStatusRes.Code, 200)
+	assert.Equal(t, getStatusRes.ConnectorStatus["state"], "PAUSED")
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// resume connector
+	resumeResp, err := connect.ResumeConnector(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, resumeResp.Code, 202)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// get the status now. Should be in RUNNING state
+	getStatusResumeRes, err := connect.GetConnectorStatus(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, getStatusResumeRes.Code, 200)
+	assert.Equal(t, getStatusResumeRes.ConnectorStatus["state"], "RUNNING")
 }
-func (suite *ConnectTestSuite)ResumeConnector(){
-	suite.Require().Equal(nil, nil)
+
+func  TestConnect_DeleteConnector(t *testing.T) {
+	// create connector
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-delete-connector",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// delete connector
+	deleteRes, err := connect.DeleteConnector(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, deleteRes.Code, 204)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	// get connector. should not exist
+	getConnectorRes, err := connect.GetConnector(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, getConnectorRes.Code, 404)
 }
-func (suite *ConnectTestSuite) DeleteConnector(){
-	suite.Require().Equal(nil, nil)
+
+func TestConnect_GetConnectorTasks(t *testing.T) {
+	connect := NewConnect(connectURL)
+	req := connect.CreateConnectorRequest(ConnectorRequest{
+		Name:   "test-get-connector-tasks",
+		Config: testConnectorConfig,
+	})
+	response, err := connect.CreateConnector(req)
+	assert.NoError(t, err)
+	assert.Equal(t, response.Code, 201)
+
+	// sleep to allow for rebalance
+	sleep()
+
+	getTaskResp, err := connect.GetConnectorTasks(req.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, getTaskResp.Code, 200)
 }
-func (suite *ConnectTestSuite) GetConnectorTasks(){
-	suite.Require().Equal(nil, nil)
+
+func TestConnect_GetConnectorTaskStatus(t *testing.T){
+	t.Skip("Test me")
 }
-func (suite *ConnectTestSuite) GetConnectorTaskStatus(){
-	suite.Require().Equal(nil, nil)
+
+func TestConnect_RestartConnectorTask(t *testing.T) {
+	t.Skip("Test me")
 }
-func (suite *ConnectTestSuite) RestartConnectorTask(){
-	suite.Require().Equal(nil, nil)
+func TestConnect_GetConnectorPlugins(t *testing.T) {
+	t.Skip("Test me")
 }
-func (suite *ConnectTestSuite) GetConnectorPlugins() {
-	suite.Require().Equal(nil, nil)
-}
-func (suite *ConnectTestSuite) ValidatePluginConfig() {
-	suite.Require().Equal(nil, nil)
+func TestConnect_ValidatePluginConfig(t *testing.T) {
+	t.Skip("Test me")
 }
